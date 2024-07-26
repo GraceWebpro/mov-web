b03494ac36e07458bad3c6c7dc3d8d904127d73d
GraceWebpro <102426261+GraceWebpro@users.noreply.github.com>
2024-07-26 11:36:32 -0700

    fourt  commit

--- src/components/home/HomeHeader.js
+++ src/components/home/HomeHeader.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./HomeHeader.css";
 import { Link } from "react-router-dom";
+import Hero from "./Hero";
 
 
 
 function HomeHeader({ user, selected }) {
     return (
         <div className='HomeHeader'>
+            <Hero />
             Hello homepage
             <Link to="about">Go to about</Link>
 
